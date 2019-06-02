86b630783881870899c8fc1ed6fa811c130d8e8f
Mario <maholp@gmail.com>
2019-06-02 21:43:49 +0200

    Steurerung und Animation
    Animation für schwarzes Schaf sowie ein neuer Versuch des Touchinputs

--- Assets/Scipts/TouchController.cs
+++ Assets/Scipts/TouchController.cs
@@ -14,29 +14,47 @@ public class TouchController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.touchCount > 0) {
+		Touch touch = Input.GetTouch (0);
 
-			Touch touch = Input.GetTouch (0);
-			Vector3 touch_Pos = Camera.main.ScreenToWorldPoint (touch.position);
+		if (Input.touchCount > 0 || touch.deltaTime > 200f)  {
 
-			if (touch_Pos.x > 0) {
-				sheepController.MoveRight ();
+//Tochinput controlling
+//
+//			Vector3 touch_Pos = Camera.main.ScreenToWorldPoint (touch.position);
+//
+//
+//
+//			if (touch_Pos.x > 0) {
+//				sheepController.MoveRight ();
+//			}
+//			else if (touch_Pos.x < 0) {
+//				sheepController.MoveLeft ();
+//					
+//			} 
+//
+//			if (touch_Pos.y > 0) {
+//				sheepController.MoveUp ();
+//			}
+//			else if (touch_Pos.y < 0) {
+//				sheepController.MoveDown ();
+//
+//			} 
+
+			//move object to touch position
+//			else  if (touch.phase = TouchPhase.Began) // if long touch 
+			{
+				// do stuff. 
+
+				// If you want to detect long press on an object, do the following.
+
+
+				Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+				GameObject sheep = GameObject.FindGameObjectWithTag ("black-sheep");
+				sheep.transform.position = new Vector3 (pos.x, pos.y, 1.8f);
+
+							
 			}
-			else if (touch_Pos.x < 0) {
-				sheepController.MoveLeft ();
-					
-			} 
 
-			if (touch_Pos.y > 0) {
-				sheepController.MoveUp ();
-			}
-			else if (touch_Pos.y < 0) {
-				sheepController.MoveDown ();
-
-			} 
-			else {
-			
-			}
 		}
 	}
 }
